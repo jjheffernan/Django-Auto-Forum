a20e303c55bbbc667b06a1fc5e5126214a336ca4
jjheffernan <jjheffernan99@gmail.com>
2022-08-05 17:53:01 -0400

    added project showcase 'projects'

--- forum_base/settings.py
+++ forum_base/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'projects',
     'test_page'
 ]
 # List of project specific apps
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'forum_base.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["forum_base/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- test_page/urls.py
+++ test_page/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from test_page import views
 
 urlpatterns = [
-    path('', views.test_page, name='test_page')
+    path('test_error/', views.test_page, name='test_page')
 ]
