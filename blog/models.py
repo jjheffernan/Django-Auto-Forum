90e14806fa70481edf0ad7d3e3017791f64455cf
jjheffernan <jjheffernan99@gmail.com>
2022-08-10 11:53:10 -0400

    installed blog app onto django main

--- blog/models.py
+++ blog/models.py
@@ -16,11 +16,28 @@ class Category(models.Model):
 
 # BLOG FORUM POST
 class Post(models.Model):  # blog style post, different from project or forum post
-    title = models.CharField(max_length=255)
-    body = models.TextField()
+    """
+    this is the Database model for a Blog-style post on the web app.
+    This is delivered differently, still uses Bootstrap like projects.
+    Essentially removes comments, feedback. More optimized for keeping track of forums.
+    """
+    title = models.CharField(max_length=255)  # same code as projects model
+    body = models.TextField()  # same code as projects model, title could be build out
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     # categories needs to be filled out
+    # add accomodations for CarFields
     categories = models.ManyToManyField('Category', related_name='posts')
 
+
+# blog comments
+class Comment(models.Model):
+    author = models.CharField(max_length=60)  # length of author comment name on post
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True) # adds comment timestamp
+    # comments cannot be edited at this time
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    # ForeignKey Defines many-to-One relationships with our Post class
+    # - first arg is model for relationship.
+    # - Second is to delete hanging comments on blog post on deletion
 
--- forum_base/settings.py
+++ forum_base/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'projects',
+    'blog',
     'test_page',
 ]
 # List of project specific apps
