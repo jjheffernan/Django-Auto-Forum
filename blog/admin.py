c5ac46fe644141ae916fe967b323cbd2f6d6c47a
jjheffernan <jjheffernan99@gmail.com>
2022-08-10 12:16:23 -0400

    added blog post management to admin site.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from blog.models import Post, Category
+
 
 # Register your models here.
+# register blog.PostAdmin
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+#
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+# Registers file to site
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+
