546f8409c9fc4e40232f250422e03f88d2e42c9a
jjheffernan <jjheffernan99@gmail.com>
2022-08-14 21:53:33 -0400

    commented alternative methods for object views, likely more compat with CBV

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,22 @@
 # django imports
-from django.shortcuts import render
-from .forms import CommentForm
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 
 # local namespace imports
 from blog.models import Post, Comment
+from blog.forms import CommentForm
 
 
 # Create your views here.
 # function based index view
 # need to abstract to class
 def blog_index(request):
-    posts = Post.objects.all().order_by('-created_on') # by changing to class oriented, this can be separated
+    posts = Post.objects.all().order_by('-created_on')  # by changing to class oriented, this can be separated
     # objects is a bad call
+    # alternative data call
+    # data = {
+    #     'posts': Post.objects.all(),
+    # }
     context = {
         'posts': posts,
     }
@@ -35,6 +40,12 @@ def blog_category(request, category):
 def blog_detail(request, pk):
     # object fetch from Post model, by private key for page
     post = Post.objects.get(pk=pk)
+    # alternative view method
+    # post = get_object_or_404(Post, id=pk)
+    # alternative data method (dict)
+    # data = {
+    #     'post': post,
+    # }
 
     form = CommentForm()
     if request.method == 'POST':
