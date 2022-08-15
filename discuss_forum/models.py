f288c5e6c9368f791864a6cfa10e7cafedc3673e
jjheffernan <jjheffernan99@gmail.com>
2022-08-14 22:10:41 -0400

    created ForumCategory model

--- discuss_forum/models.py
+++ discuss_forum/models.py
@@ -34,3 +34,37 @@ class Author(models.Model):
         if not self.slug:
             self.slug = slugify(self.fullname)
         super(Author, self).save(*args, **kwargs)
+
+
+class ForumCategory(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=400, unique=True, blank=True)
+    description = models.TextField(default='description')
+
+    class Meta:  # Create metadata to interface with other categories
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(ForumCategory, self).save(*args, **kwargs)
+
+    def get_url(self):
+        return reverse('posts', kwargs={
+            'slug': self.slug,
+        })
+
+    @property
+    def num_posts(self):
+        return ForumPost.objects.filter(categories=self).count()
+
+    @property
+    def last_post(self):
+        return ForumPost.objects.filter(categories=self).count()
+
+
+
+
