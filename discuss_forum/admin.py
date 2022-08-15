50df0ae7d3c1d1bedbf49dd2019d17c46b03a540
jjheffernan <jjheffernan99@gmail.com>
2022-08-15 01:16:53 -0400

    endabled Forum Moderation in admin panel

--- discuss_forum/admin.py
+++ discuss_forum/admin.py
@@ -8,8 +8,8 @@ from discuss_forum.models import *
 
 # Register your models here.
 
-admin.site.register()
-admin.site.register()
-admin.site.register()
-# admin.site.register(Author)
-# admin.site.register(Reply)
+admin.site.register(ForumCategory)
+admin.site.register(ForumPost)
+admin.site.register(ForumComment)
+admin.site.register(Author)
+admin.site.register(Reply)
