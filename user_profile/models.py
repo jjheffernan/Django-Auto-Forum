b23348607643b24bb36db2669c7e029288f669e3
jjheffernan <jjheffernan99@gmail.com>
2022-08-29 00:03:09 -0400

    added user profile to model. incomplete and need to migrate

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,17 @@
+# user_profile/models.py
+# django imports
 from django.db import models
+from django.contrib.auth.models import User
+
+# local namespace imports
+# none
+
 
 # Create your models here.
+# base user profile
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # profile_img = models.ImageField(default='default.jpg', upload_to='profile_pics')  # profile image integration
+
+    # def __str__(self):
+    #     return f'{self.user.} Profile'
