9616a3ec4bb1bced909f0ff5492358b05ea92a5a
jjheffernan <jjheffernan99@gmail.com>
2022-08-29 01:40:24 -0400

    added forms to user_profile to update profile picture and username. must migrate

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -6,7 +6,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 # local namespace imports
-# none
+from user_profile.models import Profile
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -17,3 +17,20 @@ class CustomUserCreationForm(UserCreationForm):
         fields = ['username', 'email', 'password1', 'password2']
         # fields = UserCreationForm.Meta.fields + ('email',)
         # in order to use the above fields line, update meta to Meta(UserCreationForm.Meta)
+
+
+# update user information, Username/email
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+# Update profile picture
+class ProfileUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = ['image']
