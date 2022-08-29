7e0fdfef8ee472ca6c8f47c6c1c6ee573e0c24a7
jjheffernan <jjheffernan99@gmail.com>
2022-08-28 23:51:19 -0400

    added extra logic to registration process

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,7 +1,9 @@
 # user_profile/views.py
+
 # django imports
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.contrib import messages
 from django.urls import reverse
 
 # local imports
@@ -22,9 +24,17 @@ def register(request):
                       {'form': CustomUserCreationForm})
     elif request.method == "POST":
         form = CustomUserCreationForm(request.POST)
+
         if form.is_valid():
             user = form.save(commit=False)  # does not immediately save
             user.backend = 'django.contrib.auth.backends.ModelBackend'  # a backend associated with user, then saves db
+
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')  # logs the registration on backend/ admin
+
             user.save()
             login(request, user)
             return redirect(reverse('dashboard'))
+        else:
+            form = CustomUserCreationForm()
+    return render(request, 'user_profile/register.html', {'form': CustomUserCreationForm})
