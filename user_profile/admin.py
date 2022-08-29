f4d7e0115cc8388c1670637bb4b37dfc6459139b
jjheffernan <jjheffernan99@gmail.com>
2022-08-28 23:58:58 -0400

    updated formatting. registerd user profiles to admin site. need to migrate

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,3 +1,11 @@
+# user_profile/admin.py
+
+# django imports
 from django.contrib import admin
 
+# local namespace imports
+from user_profile.models import Profile
+
 # Register your models here.
+
+admin.site.register(Profile)
