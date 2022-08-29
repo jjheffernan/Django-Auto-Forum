1db2871835f0ba1838a8eaeb7fbd2eb853237da6
jjheffernan <jjheffernan99@gmail.com>
2022-08-29 01:51:14 -0400

    refactored url structure to be more distinct

--- blog/urls.py
+++ blog/urls.py
@@ -9,11 +9,11 @@ from blog import views
 
 urlpatterns = [
     path('index/', views.BlogIndexView.as_view(), name='blog_index'),
-    path('<int:pk>/', views.BlogDetailView.as_view(), name='blog_detail'),
+    path('post/<int:pk>/', views.BlogDetailView.as_view(), name='blog_detail'),
     path('<category>/', views.BlogCategoryView.as_view(), name='blog_category')
-    # path('create/', views.AddBlog.as_view(), name='create_blog'),
-    # path('edit/<int:pk>/', views.EditBlog.as_view(), name='edit_blog'),
-    # path('delete/<int:pk>/', views.DeleteBlog.as_view(), name='delete_blog'),
+    # path('post/create/', views.AddBlog.as_view(), name='create_blog'),
+    # path('post/<int:pk>/edit/', views.EditBlog.as_view(), name='edit_blog'),
+    # path('post/<int:pk>/delete/', views.DeleteBlog.as_view(), name='delete_blog'),
 ]
 
 # path('<slug:blog_post>/', views.BlogDetailView.as_view(), name='blog_detail'),
