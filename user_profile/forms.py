16a7755167bfe9a2868c58e7db1845b265411ee1
jjheffernan <jjheffernan99@gmail.com>
2022-08-29 13:47:57 -0400

    created registration form. Need to migrate

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -83,7 +83,10 @@ class PwdResetForm(PasswordResetForm):
             raise forms.ValidationError('Invalid email address, please register.')
         return email
 
+# -- Accont Creation Forms --
 
+
+# Admin/Custom User Creation (non-standard roles)
 class CustomUserCreationForm(UserCreationForm):
     # This will eventually will be for admin creation and management of user profiles
     email = forms.EmailField()
@@ -114,3 +117,55 @@ class ProfileUpdateForm(forms.ModelForm):
         widgets = {
             'bio': forms.Textarea(attrs={'class': 'form-control', 'rows': '5'})
         }
+
+
+class RegistrationForm(forms.ModelForm):
+
+    username = forms.CharField(label='Enter Username', min_length=4, max_length=50, help_text='Required')
+    email = forms.EmailField(max_length=100, help_text='Required',
+                             error_messages={
+                                 'required': 'Invalid email, please put in an email to proceed'
+                             })
+
+    password1 = forms.CharField(label='Enter Password', widget=forms.PasswordInput())
+    password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name')
+
+    def clean_username(self):
+        username = self.cleaned_data['username'].lower()
+        r = User.objects.filter(username=username)
+        if r.count():
+            raise ValidationError("Username already exists")
+        return username
+
+    def clean_pwd2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise ValidationError('Passwords do not match.')
+        return cd['password2']
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise ValidationError('Username is taken')
+        return email
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update(
+            {'class': 'form-control mb-3',
+             'placeholder': 'Username'})
+        self.fields['email'].widget.attrs.update(
+            {'class': 'form-control mb-3',
+             'placeholder': 'Email',
+             'name': 'email',
+             'id': 'id_email'})
+        self.fields['password'].widget.attrs.update(
+            {'class': 'form-control',
+             'placeholder': 'Password'})
+        self.fields['password2'].widget.attrs.update(
+            {'class': 'form-control',
+             'placeholder': 'Repeat Password'})
