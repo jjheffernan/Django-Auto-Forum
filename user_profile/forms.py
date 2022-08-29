ccad6ad98b1dcfdde03ec3fc8567e0b518972f36
jjheffernan <jjheffernan99@gmail.com>
2022-08-28 23:45:51 -0400

    expanded user creation forms to be more robust, need to make migrations

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -1,7 +1,19 @@
 # user_profile/forms.py
 
+# django imports
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+# local namespace imports
+# none
+
 
 class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        fields = UserCreationForm.Meta.fields + ('email',)
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        # fields = UserCreationForm.Meta.fields + ('email',)
+        # in order to use the above fields line, update meta to Meta(UserCreationForm.Meta)
